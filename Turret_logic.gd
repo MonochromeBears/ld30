3f1077eb1b473fcfcd84adabe62f62800467700a
Lancer411 <skidivay@gmail.com>
2014-08-24 12:57:44 +0700

    Laser death rays for turrets

--- Turret_logic.gd
+++ Turret_logic.gd
@@ -5,33 +5,59 @@ extends Node2D
 # var a=2
 # var b="textvar"
 
+var homeless = preload("res://FallingBum.gd")
+var Laser = preload("res://laser.xml")
 var LockOnTarget # Turred aimed at this
 var Targets = []
 var Turret
+var cooldown = 10 #frames cooldown
+var cantshoot = 0
 
+func offset_vector(VectorOne, VectorTwo):
+	var dots = []
+	var dx = VectorTwo.x - VectorOne.x
+	var dy = VectorTwo.y - VectorOne.y
+	return Vector2(Vector2(dx,dy).length(),0) #.length()/2
+	#dx = dx/len
+	#dy = dy/len
+	#while(len > 0):
+	#	len -= 1
+	#	dots.push_back(Vector2(VectorOne.x + dx*len, VectorOne.y + dy*len))
+	#return Vector2Array(dots)
+	
 func _ready():
 	Turret = get_child("Telo").get_child("turel")
 	set_fixed_process(true)
 	pass
 	
 func _fixed_process(delta):
-	if(!LockOnTarget):
-		if(Targets.size()):
-			LockOnTarget = Targets[0]
+	if(cantshoot):
+		cantshoot -= 1
 	else:
-		var VectorGun = Turret.get_pos()
-		var VectorBum = LockOnTarget.get_pos()
-		var Angle = VectorGun.angle_to(VectorBum)
-		Turret.set_rot(Angle)
-		
-		Targets.erase(LockOnTarget)
-		LockOnTarget.get_parent().remove_and_delete_child(LockOnTarget)
-		LockOnTarget = null
+		if(!LockOnTarget):
+			if(Targets.size()):
+				LockOnTarget = Targets[0]
+		else:
+			cantshoot = cooldown
+			var VectorGun = get_pos()
+			var VectorBum = LockOnTarget.get_pos()
+			var Angle = VectorGun.angle_to_point(VectorBum)
+			Turret.set_rot(Angle)
+			var lazor = Laser.instance()
+			lazor.set_emission_half_extents(offset_vector(VectorGun,VectorBum))
+			lazor.set_emitting(true)
+			lazor.set_rot(Angle - (3.14 / 2))
+			add_child(lazor)
+			Targets.erase(LockOnTarget)
+			LockOnTarget.get_parent().remove_and_delete_child(LockOnTarget)
+			LockOnTarget = null
 		
 	
 func _on_Area2D_body_enter( body ):
-	Targets.push_back(body)
+	if body extends homeless:
+		Targets.push_back(body)
 
 func _on_Area2D_body_exit( body ):
-	if(Targets.find(body)):
-		Targets.erase(body)
+	if body extends homeless:
+		if(Targets.find(body)):
+			Targets.erase(body)
