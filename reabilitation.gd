715a7684a18754b436685d5fd1513294d40d61f9
Vladislav Bezverkhiy <bezverhiy@a2design.biz>
2014-08-24 16:18:19 +0700

    Added destroing to centers

--- reabilitation.gd
+++ reabilitation.gd
@@ -2,6 +2,7 @@
 extends Node2D
 var reab
 var counter = 0
+var health = 5
 var homeless = preload("res://FallingBum.gd")
 # member variables here, example:
 # var a=2
@@ -29,4 +30,7 @@ func _on_Area2D_body_enter( body ):
 		body.get_parent().remove_and_delete_child(body)
 		parent.add_credits(10)
 		parent.add_score(20)
+		if health == 0:
+			queue_free()
+		health -= 1
 	pass # replace with function body
